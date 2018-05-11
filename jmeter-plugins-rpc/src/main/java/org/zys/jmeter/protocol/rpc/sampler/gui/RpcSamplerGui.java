3ffb435674d6a634c13b5bf5fc0821480d9e751f
朱永生 <YongShengZhu@sf-express.com>
2018-05-11 16:18:00 +0800

    V1.0RPC接口改用预解析。

--- jmeter-plugins-rpc/src/main/java/org/zys/jmeter/protocol/rpc/sampler/gui/RpcSamplerGui.java
+++ jmeter-plugins-rpc/src/main/java/org/zys/jmeter/protocol/rpc/sampler/gui/RpcSamplerGui.java
@@ -1,5 +1,6 @@
 package org.zys.jmeter.protocol.rpc.sampler.gui;
 
+import junit.framework.TestCase;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jmeter.gui.util.HorizontalPanel;
 import org.apache.jmeter.gui.util.JSyntaxTextArea;
@@ -9,12 +10,17 @@ import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.gui.JLabeledTextField;
+import org.apache.jorphan.reflect.ClassFilter;
+import org.apache.jorphan.reflect.ClassFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zys.jmeter.protocol.rpc.sampler.RpcSampler;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,19 +30,24 @@ import java.util.Map;
 /**
  * Created by 01369755 on 2018/3/26.
  */
-public class RpcSamplerGui extends AbstractSamplerGui {
+public class RpcSamplerGui extends AbstractSamplerGui implements ActionListener {
 
     private static final Logger log = LoggerFactory.getLogger(RpcSamplerGui.class);
 
-    private static Map<String, List<String>> methods = new HashMap<>();
+    private static Map<String, List<String>> methodNames = new HashMap<>();
 
     private JLabeledTextField protocol;
     private JLabeledTextField host;
     private JLabeledTextField port;
-    private JLabeledTextField interfaceCls;
     private JLabeledTextField version;
-    private JComboBox<String> method;
     private JSyntaxTextArea args;
+    private JComboBox<String> classNameCombo;
+    private JComboBox<String> methodNameCombo;
+
+
+    private static final String[] SPATHS = new String[]{
+            JMeterUtils.getJMeterHome() + "/lib/dubbo/" //$NON-NLS-1$
+    };
 
     public RpcSamplerGui() {
         init();
@@ -66,15 +77,18 @@ public class RpcSamplerGui extends AbstractSamplerGui {
         testElement.setProperty(RpcSampler.PROTOCOL, protocol.getText());
         testElement.setProperty(RpcSampler.HOST, host.getText());
         testElement.setProperty(RpcSampler.PORT, port.getText());
-        testElement.setProperty(RpcSampler.INTERFACE_CLASS, interfaceCls.getText());
-        if (method.getSelectedItem() != null) {
-            testElement.setProperty(RpcSampler.METHOD, method.getSelectedItem().toString());
+        if (classNameCombo.getSelectedItem() != null) {
+            testElement.setProperty(RpcSampler.INTERFACE_CLASS, classNameCombo.getSelectedItem().toString());
+        }
+        if (methodNameCombo.getSelectedItem() != null) {
+            testElement.setProperty(RpcSampler.METHOD, methodNameCombo.getSelectedItem().toString());
         }
         testElement.setProperty(RpcSampler.VERSION, version.getText());
         testElement.setProperty(RpcSampler.ARGS, args.getText());
     }
 
     private void init() {
+        log.info(System.getProperty("java.class.path"));
         setLayout(new BorderLayout(0, 5));
         setBorder(makeBorder());
         Box box = Box.createVerticalBox();
@@ -93,14 +107,8 @@ public class RpcSamplerGui extends AbstractSamplerGui {
         host.setText(element.getPropertyAsString(RpcSampler.HOST));
         port.setText(element.getPropertyAsString(RpcSampler.PORT));
         version.setText(element.getPropertyAsString(RpcSampler.VERSION));
-        interfaceCls.setText(element.getPropertyAsString(RpcSampler.INTERFACE_CLASS));
-        method.removeAllItems();
-        if (StringUtils.isNotEmpty(interfaceCls.getText())) {
-            for (String m : getMetods(interfaceCls.getText())) {
-                method.addItem(m);
-            }
-        }
-        method.setSelectedItem(element.getPropertyAsString(RpcSampler.METHOD));
+        classNameCombo.setSelectedItem(element.getPropertyAsString(RpcSampler.INTERFACE_CLASS));
+        methodNameCombo.setSelectedItem(element.getPropertyAsString(RpcSampler.METHOD));
         args.setInitialText(element.getPropertyAsString(RpcSampler.ARGS));
     }
 
@@ -109,8 +117,8 @@ public class RpcSamplerGui extends AbstractSamplerGui {
         protocol.setText("");
         host.setText("");
         port.setText("");
-        interfaceCls.setText("");
-        method.setSelectedIndex(-1);
+        classNameCombo.setSelectedIndex(-1);
+        methodNameCombo.setSelectedIndex(-1);
         version.setText("");
         args.setInitialText("");
         args.setCaretPosition(0);
@@ -118,8 +126,7 @@ public class RpcSamplerGui extends AbstractSamplerGui {
 
     private JPanel creatRequestPanel() {
         JPanel requestPanel = new VerticalPanel();
-        requestPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                "Dubbo请求"));
+        requestPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Dubbo请求"));
         requestPanel.add(createInterfacePanel(), BorderLayout.NORTH);
         requestPanel.add(createArgsPanel(), BorderLayout.CENTER);
         return requestPanel;
@@ -132,19 +139,33 @@ public class RpcSamplerGui extends AbstractSamplerGui {
         panel.add(JTextScrollPane.getInstance(args), BorderLayout.CENTER);
         return panel;
     }
+
     private JPanel createInterfacePanel() {
-        interfaceCls = new JLabeledTextField(" 接口：", 40);
-        method = new JComboBox<>();
-        JLabel label = new JLabel(" 方法:  ");
-        label.setLabelFor(method);
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(label, BorderLayout.WEST);
-        panel.add(method, BorderLayout.CENTER);
-        version = new JLabeledTextField("  版本：", 7);
+        try {
+            classNameCombo = new JComboBox(ClassFinder.findClasses(SPATHS,
+                    new InterfaceFilter("Service", "RestService")).toArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        classNameCombo.addActionListener(this);
+        JLabel classLabel = new JLabel(" 接口:  ");
+        classLabel.setLabelFor(classNameCombo);
+        JPanel classPanel = new JPanel(new BorderLayout());
+        classPanel.add(classLabel, BorderLayout.WEST);
+        classPanel.add(classNameCombo, BorderLayout.CENTER);
+
+        methodNameCombo = new JComboBox<>();
+        JLabel methodLabel = new JLabel(" 方法:  ");
+        methodLabel.setLabelFor(methodNameCombo);
+        JPanel methodPanel = new JPanel(new BorderLayout());
+        methodPanel.add(methodLabel, BorderLayout.WEST);
+        methodPanel.add(methodNameCombo, BorderLayout.CENTER);
+
+        version = new JLabeledTextField(" 版本：", 7);
 
         JPanel webServerPanel = new HorizontalPanel();
-        webServerPanel.add(interfaceCls, BorderLayout.WEST);
-        webServerPanel.add(panel, BorderLayout.CENTER);
+        webServerPanel.add(classPanel, BorderLayout.WEST);
+        webServerPanel.add(methodPanel, BorderLayout.CENTER);
         webServerPanel.add(version, BorderLayout.EAST);
         return webServerPanel;
     }
@@ -155,34 +176,87 @@ public class RpcSamplerGui extends AbstractSamplerGui {
         port = new JLabeledTextField(JMeterUtils.getResString("web_server_port"), 7); // $NON-NLS-1$
 
         JPanel webServerPanel = new HorizontalPanel();
-        webServerPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                "服务信息"));
+        webServerPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "服务信息"));
         webServerPanel.add(protocol);
         webServerPanel.add(host);
         webServerPanel.add(port);
         return webServerPanel;
     }
 
-    private List<String> getMetods(String interfaceCls) {
-        if (!methods.containsKey(interfaceCls)) {
+    private List<String> getMethodNames(String interfaceCls) {
+        if (!methodNames.containsKey(interfaceCls)) {
             List<String> method = new ArrayList();
             if (StringUtils.isNotEmpty(interfaceCls)) {
                 try {
-                    for (Method m : Class.forName(interfaceCls).getDeclaredMethods()){
+                    for (Method m : Class.forName(interfaceCls).getDeclaredMethods()) {
                         StringBuffer sb = new StringBuffer(m.getName()).append("(");
                         Class[] pts = m.getParameterTypes();
-                        for (Class pt : pts){
-                            sb.append(pt.getName()).append(",");
+                        if (null != pts && pts.length > 0) {
+                            for (Class pt : pts) {
+                                sb.append(pt.getName()).append(",");
+                            }
+                            sb.deleteCharAt(sb.lastIndexOf(","));
                         }
-                        sb.deleteCharAt(sb.lastIndexOf(",")).append(")");
+                        sb.append(")");
                         method.add(sb.toString());
                     }
                 } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                    log.error("class {} not found!", interfaceCls);
                 }
-                methods.put(interfaceCls, method);
+                methodNames.put(interfaceCls, method);
             }
         }
-        return methods.get(interfaceCls);
+        return methodNames.get(interfaceCls);
     }
+
+    private void setupMethods() {
+        methodNameCombo.removeAllItems();
+        if (null != classNameCombo.getSelectedItem()) {
+            String className = classNameCombo.getSelectedItem().toString();
+            if (className != null) {
+                List<String> names = getMethodNames(className);
+                for (String name : names) {
+                    methodNameCombo.addItem(name);
+                }
+                methodNameCombo.repaint();
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        if (evt.getSource() == classNameCombo) {
+            setupMethods();
+        }
+    }
+
+
+    private static class InterfaceFilter implements ClassFilter {
+        private final String contains; // class name should contain this string
+        private final String notContains; // class name should not contain this string
+        private final ClassLoader contextClassLoader
+                = Thread.currentThread().getContextClassLoader();
+
+        InterfaceFilter(String contains, String notContains) {
+            this.contains = contains;
+            this.notContains = notContains;
+        }
+
+        @Override
+        public boolean accept(String className) {
+            if (contains != null && !className.contains(contains)) {
+                return false; // It does not contain a required string
+            }
+            if (notContains != null && className.contains(notContains)) {
+                return false; // It contains a banned string
+            }
+            try {
+                return Class.forName(className, false, contextClassLoader).isInterface();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
 }
