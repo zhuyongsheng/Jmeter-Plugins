20d357c139a12869deaa8e897ed256fc66745790
朱永生 <YongShengZhu@sf-express.com>
2018-05-30 11:01:20 +0800

    V1.0 dubbo请求增加多线程时，每个线程使用独立的Reference

--- jmeter-plugins-rpc/src/main/java/org/zys/jmeter/protocol/rpc/sampler/RpcSampler.java
+++ jmeter-plugins-rpc/src/main/java/org/zys/jmeter/protocol/rpc/sampler/RpcSampler.java
@@ -8,12 +8,11 @@ import com.google.gson.GsonBuilder;
 import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.threads.JMeterVariables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by 01369755 on 2018/3/24.
@@ -27,9 +26,6 @@ public class RpcSampler extends AbstractSampler {
     private static Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").setPrettyPrinting().create();
     private static ApplicationConfig DUBBOSAMPLER = new ApplicationConfig("dubboSampler");
     private static int TIMEOUT = 5000;
-
-    private static Map<String, ReferenceConfig> clients = new HashMap();
-
 
     public static String PROTOCOL = "protocol";
     public static String HOST = "host";
@@ -56,7 +52,7 @@ public class RpcSampler extends AbstractSampler {
         interfaceCls = getPropertyAsString(INTERFACE_CLASS);
         version = getPropertyAsString(VERSION).trim();
         group = getPropertyAsString(GROUP).trim();
-        args = getPropertyAsString(ARGS).trim().replace("\n", "").replace("\t","");
+        args = getPropertyAsString(ARGS).trim().replace("\n", "").replace("\t", "");
         methodInfo = getPropertyAsString(METHOD);
     }
 
@@ -65,7 +61,7 @@ public class RpcSampler extends AbstractSampler {
         res.setSampleLabel(getName());
         try {
             init();
-            res.setSamplerData(interfaceCls + "." + methodInfo + "?\n"  + args);
+            res.setSamplerData(interfaceCls + "." + methodInfo + "?\n" + args);
             res.sampleStart();
             res.setResponseData(visitDubboService(), "UTF-8");
             res.setResponseCode("0");
@@ -88,22 +84,28 @@ public class RpcSampler extends AbstractSampler {
         ReferenceConfig ref = getReference(protocol, host, port, interfaceCls, version, group);
 
         Class paramType = Class.forName(methodInfo.substring(methodInfo.indexOf("(") + 1, methodInfo.indexOf(")")));
-        Method method = Class.forName(interfaceCls).getDeclaredMethod(methodInfo.substring(0,methodInfo.indexOf("(")), paramType);
+        Method method = Class.forName(interfaceCls).getDeclaredMethod(methodInfo.substring(0, methodInfo.indexOf("(")), paramType);
         return GSON.toJson(method.invoke(ref.get(), GSON.fromJson(args, paramType)));
     }
 
+    //    为以后多参数扩展做准备
     private Class[] getparamTypes(String paramTypesList) throws ClassNotFoundException {
         String[] paramTypes = paramTypesList.split(",");
         Class[] paramType = new Class[paramTypes.length];
-        for (int i=0; i < paramTypes.length; i++){
+        for (int i = 0; i < paramTypes.length; i++) {
             paramType[i] = Class.forName(paramTypes[i]);
         }
         return paramType;
     }
 
-    private ReferenceConfig getReference(String protocol, String host, String port, String interfaceCls, String version, String group) throws Exception{
+    /*Jmeter插件设计时，对于中间件（如redis，Hbase..）一般采用JMeterProperty，
+    因为中间件对于一套SUT来说，信息是确定的，是全局的，并且一般只用来做信息验证，不需要区分线程；
+    对于SUT对外接口（如RPC）则需要采用JMeterVariables，以在多线程时，能够更好的模拟多个用户。*/
+    private ReferenceConfig getReference(String protocol, String host, String port, String interfaceCls, String version, String group) throws Exception {
         StringBuffer key = new StringBuffer(host).append("_").append(interfaceCls).append("_").append(version).append("_").append(group);
-        if (!clients.containsKey(key.toString())){
+        JMeterVariables variables = getThreadContext().getVariables();
+        Object object = variables.getObject(key.toString());
+        if (null == object) {
             ReferenceConfig ref = new ReferenceConfig();
             ref.setApplication(DUBBOSAMPLER);
             ref.setInterface(interfaceCls);
@@ -111,18 +113,19 @@ public class RpcSampler extends AbstractSampler {
             ref.setGroup(group);
             ref.setTimeout(TIMEOUT);
             switch (protocol) {
-                case "dubbo" :
+                case "dubbo":
                     ref.setUrl(new StringBuffer(protocol).append("://").append(host).append(":").append(port).append("/").append(interfaceCls).toString());
                     break;
-                case "zookeeper" :
-                    RegistryConfig registryConfig= new RegistryConfig(new StringBuffer(protocol).append("://").append(host).append(":").append(port).toString());
+                case "zookeeper":
+                    RegistryConfig registryConfig = new RegistryConfig(new StringBuffer(protocol).append("://").append(host).append(":").append(port).toString());
                     ref.setRegistry(registryConfig);
                     break;
-                default :
+                default:
                     throw new Exception("unsupported protocol.");
             }
-            clients.put(key.toString(), ref);
+            variables.putObject(key.toString(), ref);
+            return ref;
         }
-        return clients.get(key.toString());
+        return (ReferenceConfig) object;
     }
 }
